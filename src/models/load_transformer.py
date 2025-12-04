165259fca94517d69d27b8bc66a004c55b47a5a1
hari3mp <harisasaif@gmail.com>
2025-12-04 09:36:58 -0800

    load

--- src/models/load_transformer.py
+++ src/models/load_transformer.py
@@ -4,7 +4,7 @@ import torch.utils.data as data
 from src.models.transformer import TransactionClassifier, TransactionDataset
 
 def load_transformer(model_file, df, X_test, y_test):
-    checkpoint = torch.load(model_file)
+    checkpoint = torch.load(model_file, weights_only=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
 
     vocab_map = checkpoint['vocab_map']
