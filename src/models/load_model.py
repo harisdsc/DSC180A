c6c6feb2fb64e9f473680216fe3df20b89b64d66
hari3mp <harisasaif@gmail.com>
2025-12-04 02:40:11 -0800

    catboost

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -7,6 +7,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 
 from src.models.load_data import load_data
+from src.models.catboost.catboost import model as catboost_model
 
 def load_model(config):
     with open(config) as f:
@@ -21,54 +22,24 @@ def load_model(config):
     X = df.drop(columns=['posted_date', 'category', 'memo'])
     y = df['category']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    text_cols = ['clean_memo']
-    cat_cols = ['day_of_week', 'month', 'quarter', 'whole_dollar', 'prism_consumer_id', 'prism_account_id']    
-    
 
     if selected_model == 'Transformer':
             ...
     else:
         if selected_model == 'CatBoost':
-            # Initialize Catboost
-            model = CatBoostClassifier(
-                iterations=10_000,
-                learning_rate=0.05,
-                depth=6,
-                loss_function='MultiClass',
-                eval_metric='Accuracy',
-                verbose=1000,
-                early_stopping_rounds=100,
-                task_type='GPU',
-                devices='0:1',
-                cat_features=cat_cols, 
-                text_features=text_cols,
-                # auto_class_weights='Balanced',
-                # text_processing=["NaiveBayes+Word,BiGram|BoW+Word,BiGram"]
-            )
-
+            model = catboost_model
             # Load Model
             print("Loading CatBoost Model...")
             model.load_model(model_file, format='cbm')
             preds = model.predict(X_test)
-
         elif selected_model == 'LogRegression':
             ...
              
+        # Evaluate Model
         print('Evaluating Model...')
         print(classification_report(y_test, preds))
         print('Confusion Matrix:')
         print(confusion_matrix(y_test, preds))
- 
-    
-
-    # Evaluate Model
-    print('Evaluating Model...')
-    preds = model.predict(X_test)
-    print(classification_report(y_test, preds))
-    print('Confusion Matrix:')
-    print(confusion_matrix(y_test, preds))
-        
 
 if __name__ == '__main__':
     args = sys.argv
