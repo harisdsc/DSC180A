ee340447524e7e03f0084d43a0d05c5e6ce45f0f
hari3mp <harisasaif@gmail.com>
2025-12-04 06:40:05 -0800

    build

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -1,13 +1,11 @@
-import numpy as np
-import pandas as pd
-import json
-import sys
-from catboost import CatBoostClassifier
+
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
+from src.models.catboost.catboost import model as catboost_model
+import json
+import sys
 
 from src.models.load_data import load_data
-from src.models.catboost.catboost import model as catboost_model
 
 def load_model(config):
     with open(config) as f:
@@ -27,7 +25,7 @@ def load_model(config):
     # Load model
     if selected_model == 'CatBoost':
         model = catboost_model
-        print("Loading CatBoost Model...")
+        print("Loading CatBoost...")
         model.load_model(model_file, format='cbm')
         preds = model.predict(X_test)
     elif selected_model == 'LogRegression':
@@ -36,7 +34,7 @@ def load_model(config):
         ...
              
     # Evaluate Model
-    print('Evaluating Model...')
+    print('Evaluating model...')
     print('Classification Report:')
     print(classification_report(y_test, preds))
     print('Confusion Matrix:')
