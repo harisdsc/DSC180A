48f35bfa6828b0ec1134bf4c56743cf799793729
hari3mp <harisasaif@gmail.com>
2025-12-04 01:49:24 -0800

    train

---
+++ src/models/train_model.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import time
+import json
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+from src.models.load_data import load_data
+from src.models.catboost.catboost import train_catboost
+    
+def train_model(config):
+    with open(config) as f:
+        config = json.load(f)
+
+    output_file = config['output_file']
+    selected_model = config['model']
+    
+    # Load Data + Feature Engineering
+    df = load_data()
+    
+    # Split data
+    X = df.drop(columns=['posted_date', 'category', 'memo'])
+    y = df['category']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    if selected_model == 'Transformer':
+            ...
+    else:
+        if selected_model == 'CatBoost':
+            model = train_catboost(X_train, y_train, X_test, y_test, output_file)
+            preds = model.predict(X_test)
+        elif selected_model == 'LogRegression':
+             ...
+             
+        print('Evaluating Model...')
+        print(classification_report(y_test, preds))
+        print('Confusion Matrix:')
+        print(confusion_matrix(y_test, preds))
+        
+        
+if __name__ == '__main__':
+    args = sys.argv
+    config = args[1] if len(args) > 1 else 'configs/models/train.json'
+    train_model(config)
+    
